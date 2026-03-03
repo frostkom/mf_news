5ad54830f19c3d61b8290f2bffb4a9379fc4b0d7
frostkom <martin.fors@frostkom.se>
2026-03-03 17:02:32 +0100

    Update grids

--- include/classes.php
+++ include/classes.php
@@ -10,38 +10,126 @@ class mf_news
 
 		if(!isset($attributes['news_amount'])){			$attributes['news_amount'] = 6;}
 		if(!isset($attributes['news_categories'])){		$attributes['news_categories'] = [];}
+		
+		$arr_out = $arr_categories = [];
+		$query_join = $query_where = "";
 
-		$plugin_base_include_url = plugins_url()."/mf_base/include/";
-		mf_enqueue_style('style_base_grid_columns', $plugin_base_include_url."style_grid_columns.php");
+		if(count($attributes['news_categories']) > 0)
+		{
+			$query_join .= " INNER JOIN ".$wpdb->term_relationships." ON ".$wpdb->posts.".ID = ".$wpdb->term_relationships.".object_id INNER JOIN ".$wpdb->term_taxonomy." USING (term_taxonomy_id)";
+			$query_where .= " AND term_id IN('".implode("','", $attributes['news_categories'])."')";
+		}
 
-		$out = "<div".parse_block_attributes(array('class' => "widget news square", 'attributes' => $attributes)).">
-			<ul class='grid_columns'>";
+		$result = $wpdb->get_results($wpdb->prepare("SELECT ID, post_title, post_excerpt, post_content, post_date FROM ".$wpdb->posts.$query_join." WHERE post_type = %s AND post_status = %s".$query_where." ORDER BY post_date DESC LIMIT 0, ".esc_sql($attributes['news_amount']), 'post', 'publish'));
 
-				$arr_categories = [];
+		foreach($result as $r)
+		{
+			$post_id = $r->ID;
+			$post_title = $r->post_title;
+			$post_excerpt = $r->post_excerpt;
+			$post_content = $r->post_content;
+			$post_date = $r->post_date;
 
-				$query_join = $query_where = "";
+			if(count($attributes['news_categories']) != 1)
+			{
+				$arr_categories = get_the_category($post_id);
+			}
 
-				if(count($attributes['news_categories']) > 0)
+			$post_url = get_permalink($post_id);
+			$post_image = get_the_post_thumbnail_url($post_id, 'large'); // medium / large / full
+
+			if($post_image != '')
+			{
+				$post_image = "<img src='".$post_image."' alt='".$post_title."'>";
+			}
+
+			else
+			{
+				$post_image = apply_filters('get_image_fallback', "");
+			}
+
+			$out_temp = "<li>
+				<div class='image'><a href='".$post_url."'>".$post_image."</a></div>
+				<div class='content'>
+					<a href='".$post_url."'>".$post_title."</a>" // (".var_export($attributes['news_categories'], true).")
+					."<div class='meta'>";
+
+						foreach($arr_categories as $arr_category)
+						{
+							if($arr_category->cat_name != __("Uncategorized", 'lang_news'))
+							{
+								$out_temp .= "<span>".$arr_category->cat_name."</span>";
+							}
+						}
+
+						$out_temp .= "<span class='grey'>".format_date($post_date)."</span>
+					</div>
+					<p class='text'>";
+
+						if($post_excerpt != '')
+						{
+							$out_temp .= $post_excerpt;
+						}
+
+						else
+						{
+							$out_temp .= shorten_text(array('string' => strip_tags($post_content), 'limit' => 120));
+						}
+
+					$out_temp .= "</p>
+					<div class='wp-block-button'>
+						<a href='".$post_url."' class='wp-block-button__link'>".__("Read More", 'lang_news')."</a>
+					</div>
+				</div>
+			</li>";
+
+			$arr_out[] = $out_temp;
+		}
+
+		if(count($arr_out) > 0)
+		{
+			$plugin_base_include_url = plugins_url()."/mf_base/include/";
+			mf_enqueue_style('style_base_grid_columns', $plugin_base_include_url."style_grid_columns.php");
+
+			$out = "<div".parse_block_attributes(array('class' => "widget news square", 'attributes' => $attributes)).">
+				<ul class='grid_columns".(count($arr_out) < 3 ? " grid_grow" : "")."'>"
+					.implode("", $arr_out)
+				."</ul>
+			</div>";
+		}
+
+		return $out;
+	}
+
+	function block_render_promote_callback($attributes)
+	{
+		global $wpdb;
+
+		if(!isset($attributes['promote_include'])){			$attributes['promote_include'] = [];}
+
+		if(count($attributes['promote_include']) > 0)
+		{
+			$arr_out = [];
+
+			$result = $wpdb->get_results($wpdb->prepare("SELECT ID, post_title, post_content FROM ".$wpdb->posts." WHERE post_type = %s AND post_status = %s AND ID IN('".implode("','", $attributes['promote_include'])."') ORDER BY menu_order ASC", 'page', 'publish'));
+
+			foreach($result as $r)
+			{
+				$post_id = $r->ID;
+				$post_title = $r->post_title;
+				$post_content = $r->post_content;
+
+				$out_temp = "";
+
+				if(strlen($post_content) < 60 && preg_match("/youtube\.com|youtu\.be/i", $post_content))
 				{
-					$query_join .= " INNER JOIN ".$wpdb->term_relationships." ON ".$wpdb->posts.".ID = ".$wpdb->term_relationships.".object_id INNER JOIN ".$wpdb->term_taxonomy." USING (term_taxonomy_id)";
-					$query_where .= " AND term_id IN('".implode("','", $attributes['news_categories'])."')";
+					$out_temp .= "<li>
+						<div class='video'>".apply_filters('the_content', $post_content)."</div>
+					</li>";
 				}
 
-				$result = $wpdb->get_results($wpdb->prepare("SELECT ID, post_title, post_excerpt, post_content, post_date FROM ".$wpdb->posts.$query_join." WHERE post_type = %s AND post_status = %s".$query_where." ORDER BY post_date DESC LIMIT 0, ".esc_sql($attributes['news_amount']), 'post', 'publish'));
-
-				foreach($result as $r)
+				else
 				{
-					$post_id = $r->ID;
-					$post_title = $r->post_title;
-					$post_excerpt = $r->post_excerpt;
-					$post_content = $r->post_content;
-					$post_date = $r->post_date;
-
-					if(count($attributes['news_categories']) != 1)
-					{
-						$arr_categories = get_the_category($post_id);
-					}
-
 					$post_url = get_permalink($post_id);
 					$post_image = get_the_post_thumbnail_url($post_id, 'large'); // medium / large / full
 
@@ -55,105 +143,30 @@ class mf_news
 						$post_image = apply_filters('get_image_fallback', "");
 					}
 
-					$out .= "<li>
-						<div class='image'><a href='".$post_url."'>".$post_image."</a></div>
-						<div class='content'>
-							<a href='".$post_url."'>".$post_title."</a>" // (".var_export($attributes['news_categories'], true).")
-							."<div class='meta'>";
-
-								foreach($arr_categories as $arr_category)
-								{
-									if($arr_category->cat_name != __("Uncategorized", 'lang_news'))
-									{
-										$out .= "<span>".$arr_category->cat_name."</span>";
-									}
-								}
-
-								$out .= "<span class='grey'>".format_date($post_date)."</span>
-							</div>
-							<p class='text'>";
-
-								if($post_excerpt != '')
-								{
-									$out .= $post_excerpt;
-								}
-
-								else
-								{
-									$out .= shorten_text(array('string' => strip_tags($post_content), 'limit' => 120));
-								}
-
-							$out .= "</p>
-							<div class='wp-block-button'>
-								<a href='".$post_url."' class='wp-block-button__link'>".__("Read More", 'lang_news')."</a>
-							</div>
-						</div>
+					$out_temp .= "<li>
+						<a href='".$post_url."'>
+							<div class='image'>"
+								.$post_image
+								."<div class='content'><span>".$post_title."</span></div>"
+							."</div>
+						</a>
 					</li>";
 				}
 
-			$out .= "</ul>
-		</div>";
+				$arr_out[] = $out_temp;
+			}
 
-		return $out;
-	}
+			if(count($arr_out) > 0)
+			{
+				$plugin_base_include_url = plugins_url()."/mf_base/include/";
+				mf_enqueue_style('style_base_grid_columns', $plugin_base_include_url."style_grid_columns.php");
 
-	function block_render_promote_callback($attributes)
-	{
-		global $wpdb;
-
-		if(!isset($attributes['promote_include'])){			$attributes['promote_include'] = [];}
-
-		if(count($attributes['promote_include']) > 0)
-		{
-			$plugin_base_include_url = plugins_url()."/mf_base/include/";
-			mf_enqueue_style('style_base_grid_columns', $plugin_base_include_url."style_grid_columns.php");
-
-			$out = "<div".parse_block_attributes(array('class' => "widget promote square", 'attributes' => $attributes)).">
-				<ul class='grid_columns'>";
-
-					$result = $wpdb->get_results($wpdb->prepare("SELECT ID, post_title, post_content FROM ".$wpdb->posts." WHERE post_type = %s AND post_status = %s AND ID IN('".implode("','", $attributes['promote_include'])."') ORDER BY menu_order ASC", 'page', 'publish'));
-
-					foreach($result as $r)
-					{
-						$post_id = $r->ID;
-						$post_title = $r->post_title;
-						$post_content = $r->post_content;
-
-						if(strlen($post_content) < 60 && preg_match("/youtube\.com|youtu\.be/i", $post_content))
-						{
-							$out .= "<li>
-								<div class='video'>".apply_filters('the_content', $post_content)."</div>
-							</li>";
-						}
-
-						else
-						{
-							$post_url = get_permalink($post_id);
-							$post_image = get_the_post_thumbnail_url($post_id, 'large'); // medium / large / full
-
-							if($post_image != '')
-							{
-								$post_image = "<img src='".$post_image."' alt='".$post_title."'>";
-							}
-
-							else
-							{
-								$post_image = apply_filters('get_image_fallback', "");
-							}
-
-							$out .= "<li>
-								<a href='".$post_url."'>
-									<div class='image'>"
-										.$post_image
-										."<div class='content'><span>".$post_title."</span></div>"
-									."</div>
-								</a>
-							</li>";
-						}
-					}
-
-				$out .= "</ul>
-			</div>";
+				$out = "<div".parse_block_attributes(array('class' => "widget promote square", 'attributes' => $attributes)).">
+					<ul class='grid_columns".(count($arr_out) < 3 ? " grid_grow" : "")."'>"
+						.implode("", $arr_out)
+					."</ul>
+				</div>";
+			}
 		}
 
 		return $out;
